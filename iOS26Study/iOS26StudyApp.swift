b3ffd9a47c0e5e76564af4d8286ae8665f02c804
GthingkingG <21thingking@gmail.com>
2026-02-06 14:30:21 +0900

    Initial Commit

---
+++ iOS26Study/iOS26StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS26StudyApp.swift
+//  iOS26Study
+//
+//  Created by One on 2/6/26.
+//
+
+import SwiftUI
+
+@main
+struct iOS26StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
